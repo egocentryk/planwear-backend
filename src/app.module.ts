ded914e8d1cd962e4b3761f2ba074d39157cb358
egocentryk <lukasz.skowron@gmail.com>
2021-02-18 19:08:03 +0100

    @nestjs/typeorm, typeorm & pg added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './components/company/company.module';
 
 @Module({
-  imports: [CompanyModule],
+  imports: [
+    CompanyModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
