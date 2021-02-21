a24773b3d7c0e4515a41799b2e006adb8606c69a
egocentryk <lukasz.skowron@gmail.com>
2021-02-21 23:05:05 +0100

    create-user.dto.ts created

---
+++ src/components/user/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import {
+  IsEmail,
+  IsString
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  readonly username: string;
+
+  @IsString()
+  readonly firstName: string;
+
+  @IsString()
+  readonly lastName: string;
+
+  @IsEmail()
+  @IsString()
+  readonly email: string;
+}
