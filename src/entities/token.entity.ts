cb84e33720b974f3dced7f2fb0168294b87f3147
egocentryk <lukasz.skowron@gmail.com>
2021-02-21 22:49:01 +0100

    token.entity.ts created

---
+++ src/entities/token.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  Entity,
+  ManyToOne
+} from 'typeorm';
+
+import { Abstract } from '../entities/abstract.entity';
+import { User } from '../entities/user.entity';
+
+export enum TokenType {
+  EMAIL_VERIFICATION_REQUEST = 'emailVerificationToken',
+  EMAIL_CHANGE_REQUEST = 'emailChangeToken',
+  PASSWORD_CHANGE_REQUEST = 'passwordChangeToken',
+  PASSWORD_FORGOT_REQUEST = 'passwordForgotToken',
+  PASSWORD_RESET_REQUEST = 'passwordResetToken'
+}
+
+@Entity('tokens')
+export class Token extends Abstract {
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  token: string;
+
+  @Column({
+    enum: TokenType,
+    nullable: true,
+    type: 'enum'
+  })
+  type: TokenType;
+
+  @Column()
+  validTo: Date;
+}
