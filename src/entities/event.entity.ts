32ce21d8b32e212504b2cf0e7b7f424b2766707e
egocentryk <lukasz.skowron@gmail.com>
2021-02-23 16:37:39 +0100

    event entity created

---
+++ src/entities/event.entity.ts
@@ -0,0 +1,18 @@
+import {
+  Column,
+  Entity
+} from 'typeorm';
+
+import { Abstract } from '../entities/abstract.entity';
+
+@Entity('events')
+export class Event extends Abstract {
+  @Column()
+  type: string;
+
+  @Column()
+  name: string
+
+  @Column('json')
+  payload: Record<string, any>;
+}
