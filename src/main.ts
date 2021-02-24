d5c37de685cf906cf0fdde7f6578b6900791e721
egocentryk <lukasz.skowron@gmail.com>
2021-02-24 15:04:23 +0100

    swagger implemented, CLI plugin enabled

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerModule
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +20,16 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api/v1');
+
+  const options = new DocumentBuilder()
+    .setTitle('PlanWEAR')
+    .setDescription('PlanWEAR application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
