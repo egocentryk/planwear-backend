41668da821869335054989c67b1c939327b8b798
egocentryk <lukasz.skowron@gmail.com>
2021-03-09 19:18:36 +0100

    roles guard & decorator added

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,27 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return this.matchRoles(roles, user.roles);
+  }
+
+  matchRoles(roles: string[], userRoles: string[]): boolean {
+    throw new Error('Method not implemented yet.');
+  }
+}
+
