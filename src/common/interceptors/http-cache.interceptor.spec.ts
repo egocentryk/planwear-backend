31976242a4bda29da4de0676c78323b6d29c1b59
egocentryk <lukasz.skowron@gmail.com>
2021-03-09 13:06:31 +0100

    http-cache.interceptor added

---
+++ src/common/interceptors/http-cache.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { HttpCacheInterceptor } from './http-cache.interceptor';
+
+describe('HttpCacheInterceptor', () => {
+  it('should be defined', () => {
+    expect(new HttpCacheInterceptor()).toBeDefined();
+  });
+});
