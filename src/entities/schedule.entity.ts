4dec8ab47c343e754d9ac2d2112b4e29d397f7a4
egocentryk <lukasz.skowron@gmail.com>
2021-03-11 18:05:25 +0100

    schedule.entity created

---
+++ src/entities/schedule.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  Entity,
+  ManyToOne
+} from 'typeorm';
+
+import { classToPlain } from 'class-transformer';
+
+import { Abstract } from './abstract.entity';
+import { User } from './user.entity';
+
+/*
+  one of the most crucial table in our database,
+  we must know exactly when each employee is available
+*/
+@Entity('schedules')
+export class Schedule extends Abstract {
+  @ManyToOne(() => User)
+  employee: User;
+
+  @Column({
+    type: 'timestamp',
+    precision: 6,
+    nullable: true
+  })
+  from: Date;
+
+  @Column({
+    type: 'timestamp',
+    precision: 6,
+    nullable: true
+  })
+  to: Date;
+
+  toJson() {
+    return classToPlain(this);
+  }
+}
