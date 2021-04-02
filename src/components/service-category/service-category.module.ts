4bae48ace20ba9173193f3399eeabd18cde55825
egocentryk <lukasz.skowron@gmail.com>
2021-04-02 12:40:07 +0200

    feat(service-category): module initialized

--- src/components/service-category/service-category.module.ts
+++ src/components/service-category/service-category.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServiceCategoryController } from './service-category.controller';
 import { ServiceCategoryService } from './service-category.service';
+import { ServiceCategory } from '@entities/service-category.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      ServiceCategory
+    ]),
+  ],
   controllers: [ServiceCategoryController],
   providers: [ServiceCategoryService]
 })
+
 export class ServiceCategoryModule {}
