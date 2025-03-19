aae04ebf322d3324a3631c8accf585a1d98d507c
egocentryk <lukasz.skowron@gmail.com>
2025-03-19 12:26:41 +0100

    feat(#11): findAll method implemented

--- src/components/user/user.resolver.ts
+++ src/components/user/user.resolver.ts
@@ -1,10 +1,16 @@
 import { User } from '@entities/user.entity'
-import { Query, Resolver } from '@nestjs/graphql'
+import { Query, Resolver, Args } from '@nestjs/graphql'
+import { UserService } from './user.service'
+import { PaginationQueryInput } from '@common/dto/pagination-query.input'
 
 @Resolver()
 export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
   @Query(() => [User], { name: 'users' })
-  async findAll() {
-    return []
+  async findAll(
+    @Args('paginationQueryInput') paginationQueryInput: PaginationQueryInput,
+  ) {
+    return this.userService.findAll(paginationQueryInput)
   }
 }
--- src/components/user/user.service.ts
+++ src/components/user/user.service.ts
@@ -2,22 +2,22 @@ import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
-} from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { InjectRepository } from '@nestjs/typeorm';
-import { DataSource, Repository } from 'typeorm';
-import { User } from '@entities/user.entity';
-import { CreateUserDto } from './dto/create-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
-import { UpdateRoleUserDto } from './dto/update-role-user.dto';
-import { UpdateStatusUserDto } from './dto/update-status-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { PaginationQueryDto } from '@common/dto/pagination-query.dto';
-import { TokenService } from '@components/token/token.service';
-import { ApiHttpResponse } from '@enums/api-http-response.enum';
-import { TokenType } from '@enums/token-type.enum';
-import { AuthResponse } from '@interfaces/auth-response.interface';
-import * as bcrypt from 'bcryptjs';
+} from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { InjectRepository } from '@nestjs/typeorm'
+import { DataSource, Repository } from 'typeorm'
+import { User } from '@entities/user.entity'
+import { CreateUserDto } from './dto/create-user.dto'
+import { LoginUserDto } from './dto/login-user.dto'
+import { UpdateRoleUserDto } from './dto/update-role-user.dto'
+import { UpdateStatusUserDto } from './dto/update-status-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { TokenService } from '@components/token/token.service'
+import { ApiHttpResponse } from '@enums/api-http-response.enum'
+import { TokenType } from '@enums/token-type.enum'
+import { AuthResponse } from '@interfaces/auth-response.interface'
+import * as bcrypt from 'bcryptjs'
+import { PaginationQueryInput } from '@common/dto/pagination-query.input'
 
 @Injectable()
 export class UserService {
@@ -29,8 +29,8 @@ export class UserService {
     private readonly tokenService: TokenService,
   ) {}
 
-  async findAll(paginationQuery: PaginationQueryDto) {
-    const { limit, offset, order = 'DESC' } = paginationQuery;
+  async findAll(paginationQueryInput: PaginationQueryInput) {
+    const { limit, offset, order = 'DESC' } = paginationQueryInput
 
     const users = await this.userRepository.find({
       skip: offset,
@@ -38,39 +38,36 @@ export class UserService {
       order: {
         id: order,
       },
-    });
+    })
 
-    return {
-      users,
-      status: true,
-    };
+    return users
   }
 
   async findOne(id: string) {
     try {
       const user = await this.userRepository.findOne({
         where: { id },
-      });
+      })
 
-      return user;
+      return user
     } catch (error) {
-      throw new NotFoundException(`User #${id} ${ApiHttpResponse.NOT_FOUND}`);
+      throw new NotFoundException(`User #${id} ${ApiHttpResponse.NOT_FOUND}`)
     }
   }
 
   async findCurrentUser(username: string): Promise<AuthResponse> {
-    const user = await this.userRepository.findOne({ where: { username } });
-    const payload = { username };
-    const token = this.jwtService.sign(payload);
+    const user = await this.userRepository.findOne({ where: { username } })
+    const payload = { username }
+    const token = this.jwtService.sign(payload)
 
-    const { id, email } = user;
+    const { id, email } = user
 
     return {
       id,
       email,
       username,
       token,
-    };
+    }
   }
 
   async create(createUserDto: CreateUserDto) {
@@ -79,17 +76,17 @@ export class UserService {
         where: {
           email: createUserDto.email,
         },
-      });
+      })
 
       if (isEmailTaken) {
         return {
           message: ApiHttpResponse.EMAIL_TAKEN,
-        };
+        }
       }
 
-      const user = this.userRepository.create(createUserDto);
+      const user = this.userRepository.create(createUserDto)
 
-      const userInserted = await this.userRepository.save(user);
+      const userInserted = await this.userRepository.save(user)
 
       if (userInserted) {
         // insert data to token table
@@ -98,27 +95,27 @@ export class UserService {
           token: bcrypt.hashSync(user.email, 12),
           type: TokenType.EMAIL_VERIFICATION_REQUEST,
           validTo: new Date(new Date().getTime() + 60 * 60 * 24 * 1000),
-        };
+        }
 
-        this.tokenService.create(tokenInfo);
+        this.tokenService.create(tokenInfo)
       }
 
       const payload = {
         id: user.id,
         email: user.email,
         username: user.username,
-      };
+      }
 
-      const token = this.jwtService.sign(payload);
+      const token = this.jwtService.sign(payload)
 
       return {
         user: {
           ...user.toJSON(),
           token,
         },
-      };
+      }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
@@ -126,28 +123,28 @@ export class UserService {
     email,
     password,
   }: {
-    email: string;
-    password: string;
+    email: string
+    password: string
   }): Promise<LoginUserDto> {
     const user = await this.userRepository.findOne({
       where: {
         email,
       },
-    });
+    })
 
-    const isValid = user.compare(password);
+    const isValid = user.compare(password)
 
     if (!isValid) {
-      throw new UnauthorizedException(ApiHttpResponse.INVALID_CREDENTIALS);
+      throw new UnauthorizedException(ApiHttpResponse.INVALID_CREDENTIALS)
     }
 
     const payload = {
       id: user.id,
       email: user.email,
       username: user.username,
-    };
+    }
 
-    const token = this.jwtService.sign(payload);
+    const token = this.jwtService.sign(payload)
 
     /*
      * not used ATM
@@ -158,7 +155,7 @@ export class UserService {
     return {
       ...user,
       token,
-    };
+    }
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
@@ -166,18 +163,18 @@ export class UserService {
       const user = await this.userRepository.preload({
         id: id,
         ...updateUserDto,
-      });
+      })
 
-      return this.userRepository.save(user);
+      return this.userRepository.save(user)
     } catch (error) {
-      throw new NotFoundException(`User #${id} ${ApiHttpResponse.NOT_FOUND}`);
+      throw new NotFoundException(`User #${id} ${ApiHttpResponse.NOT_FOUND}`)
     }
   }
 
   async remove(id: string) {
-    const user = await this.findOne(id);
+    const user = await this.findOne(id)
 
-    return this.userRepository.remove(user);
+    return this.userRepository.remove(user)
   }
 
   async changeRole(id: string, updateRoleUserDto: UpdateRoleUserDto) {
@@ -185,11 +182,11 @@ export class UserService {
       const user = await this.userRepository.preload({
         id: id,
         ...updateRoleUserDto,
-      });
+      })
 
-      return this.userRepository.save(user);
+      return this.userRepository.save(user)
     } catch (error) {
-      throw new NotFoundException(`User #${id} ${ApiHttpResponse.NOT_FOUND}`);
+      throw new NotFoundException(`User #${id} ${ApiHttpResponse.NOT_FOUND}`)
     }
   }
 
@@ -198,11 +195,11 @@ export class UserService {
       const user = await this.userRepository.preload({
         id: id,
         ...updateStatusUserDto,
-      });
+      })
 
-      return this.userRepository.save(user);
+      return this.userRepository.save(user)
     } catch (error) {
-      throw new NotFoundException(`User #${id} ${ApiHttpResponse.NOT_FOUND}`);
+      throw new NotFoundException(`User #${id} ${ApiHttpResponse.NOT_FOUND}`)
     }
   }
 }
