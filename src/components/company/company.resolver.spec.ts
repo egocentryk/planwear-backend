19a3c0ba24c53e6869d820f72948416002f5a1b7
egocentryk <lukasz.skowron@gmail.com>
2025-03-20 20:27:45 +0100

    feat(#15): resolver & corresponding methods created

---
+++ src/components/company/company.resolver.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CompanyResolver } from './company.resolver';
+
+describe('CompanyResolver', () => {
+  let resolver: CompanyResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CompanyResolver],
+    }).compile();
+
+    resolver = module.get<CompanyResolver>(CompanyResolver);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+});
---
+++ src/components/company/dto/create-company.input.ts
@@ -0,0 +1,23 @@
+import { Field, InputType } from '@nestjs/graphql'
+import { IsArray, IsOptional, IsString, IsUUID } from 'class-validator'
+
+@InputType()
+export class CreateCompanyInput {
+  @Field(() => String)
+  @IsString()
+  readonly title: string
+
+  @Field(() => String)
+  @IsString()
+  readonly content: string
+
+  @Field(() => String)
+  @IsString()
+  readonly owner: string
+
+  @Field(() => [String], { nullable: true })
+  @IsOptional()
+  @IsArray()
+  @IsUUID('4', { each: true })
+  readonly employeeIds?: string[]
+}
